e2ef468779421c2bd3b4bdaeba9ffbd8f5cec211
Ryan Cheley <rcheley@gmail.com>
2021-06-12 19:35:44 -0700

    initial commit

---
+++ program.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+chrome_options = Options()  
+chrome_options.add_argument("--headless")
+
+driver = webdriver.Chrome(chrome_options=chrome_options)
+
+url = 'https://careers.mydohc.com/spa/listing'
+
+driver.get(url)
+
+content = driver.page_source
+soup = BeautifulSoup(content, "lxml")
+
+table = soup.find('table', class_='table table-striped')
+
+df = pd.read_html(table.prettify())[0]
+df.drop(df.tail(1).index,inplace=True)
+df.to_csv('jobs.csv')
+
