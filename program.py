b94ca5d2acb66b5b689cfdcfc5e53fd9e59ca828
Ryan Cheley <rcheley@gmail.com>
2022-03-07 07:09:20 -0800

    putting it back

--- program.py
+++ program.py
@@ -7,7 +7,7 @@ import pandas as pd
 chrome_options = Options()  
 chrome_options.add_argument("--headless")
 
-driver = webdriver.Chrome(options=chrome_options, version_main=98)
+driver = webdriver.Chrome(options=chrome_options)
 
 url = 'https://careers.mydohc.com/spa/listing'
 
